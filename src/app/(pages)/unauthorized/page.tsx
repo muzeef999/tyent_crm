67edf5ee4204076fa614153cc743e549a84d8bb5
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2025-10-14 08:21:24 +0530

    14-10-2025

--- src/app/(pages)/unauthorized/page.tsx
+++ src/app/(pages)/unauthorized/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function UnauthorizedPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
@@ -5,12 +7,12 @@ export default function UnauthorizedPage() {
       <p className="text-gray-600 mb-6">
         You don’t have permission to view this page.
       </p>
-      <a
+      <Link
         href="/"
         className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
       >
         Go Back Home
-      </a>
+      </Link>
     </div>
   );
 }
